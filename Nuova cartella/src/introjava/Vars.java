28e84cad59ad5d7ab0509a600c21f44b6f742ab9
carlo spadotto <carlospadotto.sanso@gmail.com>
2015-12-16 16:01:42 +0100

    second exam

---
+++ Nuova cartella/src/introjava/Vars.java
@@ -0,0 +1,20 @@
+package introjava;
+
+public class Vars {
+	
+	public static void main(String[] args){
+		System.out.println("Vars");
+		
+		//int
+		int i1 = 10;
+		int i2 = 15;
+		
+		System.out.println(i1 + i2);
+		
+		//boolean
+		
+		
+		
+	}
+
+}
